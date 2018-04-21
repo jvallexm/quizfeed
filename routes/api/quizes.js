dcd742f0e30eda3b79181f108a8ab8179e2b67f1
Jenny Valle <jennyvalle@Jennys-MacBook-Air.local>
2018-04-21 19:28:30 -0400

    Added quizes api routes

--- routes/api/quizes.js
+++ routes/api/quizes.js
@@ -1,17 +1,17 @@
-const router = require("express").Router();
+const router         = require("express").Router();
 const quizController = require("../../controllers/quizController");
 
 router.route("/")
-      .get(quizController.findAll)
-      .post(quizController.createOne);
+      .get(quizController.findAll)       // Finds all quizes
+      .post(quizController.createOne);   // Posts a new quiz
 
 router.route("/:id")
-      .get(quizController.findOne)
-      .put(quizController.updateData)
-      .post(quizController.editQuiz)
-      .delete(quizController.deleteOne);
+      .get(quizController.findOne)       // Finds a quiz by id
+      .put(quizController.updateData)    // Updates user data for quiz
+      .post(quizController.editQuiz)     // Updates fields of a quiz
+      .delete(quizController.deleteOne); // Deletes a quiz
 
 router.route("/user/:id")
-      .get(quizController.findAllByUser);
+      .get(quizController.findAllByUser); // Finds all quizes by a single user
 
 module.exports = router;
