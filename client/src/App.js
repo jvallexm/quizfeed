c61ddc5c358f1b82f6e848d8c37e0d94f8476dd5
Jenny Valle <jennyvalle@Jennys-MacBook-Air.local>
2018-04-23 18:34:34 -0400

    Added comments in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -28,12 +28,19 @@ class App extends React.Component{
         <div>
           <Switch>
             
+            {/* Home page */}
             <Route exact path="/"                render={ (props)=> <Home       {...props} /> } />
+            {/* Quizzes by User */}
             <Route exact path="/userQuizzes/:id" render={ (props)=> <Home       {...props} /> } />
+            {/* Overflow quiz pages */}
             <Route exact path="/page/:offset"    render={ (props)=> <Home       {...props} /> } />
+            {/* Creating a new quiz */}
             <Route exact path="/newQuiz"         render={ (props)=> <EditQuiz   {...props} /> } />
+            {/* Editing existing quiz */}
             <Route exact path="/editQuiz/:id"    render={ (props)=> <EditQuiz   {...props} /> } />
+            {/* Taking a Quiz*/}
             <Route exact path="/quiz/:id"        render={ (props)=> <Home       {...props} /> } />
+            {/* Error Page */}
             <Route exact path="/404/:code?"      render={ (props)=> <FourOhFour {...props} /> } />
 
           </Switch>
