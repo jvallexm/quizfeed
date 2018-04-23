95fec4c1d2801d076e2ac46f11d3ea7080b12d08
Jenny Valle <jennyvalle@Jennys-MacBook-Air.local>
2018-04-23 18:07:59 -0400

    changed app from a const to a class

--- client/src/App.js
+++ client/src/App.js
@@ -5,22 +5,34 @@ import EditQuiz   from "./pages/EditQuiz";
 import FourOhFour from "./pages/Error";
 
 
-const App = () => (
-  <Router>
-    <div>
-      <Switch>
-        
-        <Route exact path="/"                component={Home} />
-        <Route exact path="/userQuizzes/:id" component={Home}/>
-        <Route exact path="/page/:offset"    component={Home}/>
-        <Route exact path="/newQuiz"         component={EditQuiz}/>
-        <Route exact path="/editQuiz/:id"    component={EditQuiz}/>
-        <Route exact path="/quiz/:id"        component={Home}/>
-        <Route exact path="/404/:code?"      component={FourOhFour}/>
+/* https://tylermcginnis.com/react-router-pass-props-to-components/ */
 
-      </Switch>
-    </div>
-  </Router>
-);
+class App extends React.Component{
+
+  render(){
+
+    return(
+
+      <Router>
+        <div>
+          <Switch>
+            
+            <Route exact path="/"                component={Home} />
+            <Route exact path="/userQuizzes/:id" component={Home}/>
+            <Route exact path="/page/:offset"    component={Home}/>
+            <Route exact path="/newQuiz"         component={EditQuiz}/>
+            <Route exact path="/editQuiz/:id"    component={EditQuiz}/>
+            <Route exact path="/quiz/:id"        component={Home}/>
+            <Route exact path="/404/:code?"      component={FourOhFour}/>
+
+          </Switch>
+        </div>
+      </Router>
+
+    )
+
+  }
+  
+};
 
 export default App;
