dde6980e25e4a52c410dc59c6ddcbbae9b723afe
p2bilt <peteralan@gmail.com>
2018-04-29 16:36:52 -0400

    update question and add color picker

--- client/src/components/NewAnswer.js
+++ client/src/components/NewAnswer.js
@@ -1,16 +1,28 @@
 import React from "react";
-import { InputGroup, Card, CardBody, CardTitle, Button, Label, Input } from 'reactstrap';
+import { InputGroup, Card, CardBody, CardTitle, Button, ButtonGroup, Label, Input } from 'reactstrap';
+import "./NewAnswer.css"
 
 const NewAnswer = () => {
 
     return(
 
-                    <Card>
-            <CardBody>
-                <h5>Answer Block<Button className="close" aria-label="Close" onClick={this.toggle}><span aria-hidden="true">×</span></Button></h5>
-<Button>Add an Image</Button><Button>Add Text</Button>
-            </CardBody>
-                </Card>
+                    <div className="col-md-4">
+                        <Card className="mb-4 box-shadow">
+                            <a href="https://placeholder.com"><img className="card-img-top" src="http://via.placeholder.com/150x150"></img></a>
+                            <CardBody>
+                            <p><input name="Answer" className="answer-title" placeholder="Type Your Answer Here" onChange={this.handleChange} /></p>
+                                <div className="d-flex justify-content-between align-items-center">
+                                    <ButtonGroup className="pull-right">
+                                        <Button type="button" className="btn btn-sm btn-outline-secondary pull-right">Add an Image</Button>
+                                        <Button type="button" className="btn btn-sm btn-outline-secondary pull-right">Delete Answer</Button>
+                                    </ButtonGroup>
+                                </div>
+                            </CardBody>
+                        </Card>
+                    </div>
+
+
+
 
 
     )
