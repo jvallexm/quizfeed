6f7abdda37cfd5fa6cec400a34e78c9827bad47c
unknown <melissa@txable.com>
2018-05-02 20:21:29 -0400

    change QuizListItem to reusable card component

--- client/src/components/QuizListItem.js
+++ client/src/components/QuizListItem.js
@@ -1,26 +1,27 @@
 import React        from "react";
-import { Row, Col } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle, CardSubTitel, Button} from 'reactstrap';
 
 
 const QuizListItem = () =>{
 
 
-  return(
+  return (
+    <div>
+      <Card>
+        <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+        <CardImgOverlay>
+          <Button color="info">Rating</Button>{' '}
+          <Button color="info">#Comments</Button>{' '}
+        </CardImgOverlay>
+        <CardBody>
+          <CardTitle>(Quiz title)</CardTitle>
+          <CardSubtitle>By (QuizAuthor)</CardSubtitle>
+          <CardText>(Quick Quiz Description)</CardText>
+          <Button>Play This Quiz</Button>
+        </CardBody>
+      </Card>
+    </div>
+  );
+};
 
-    <Row>
-        <Col md="4">
-          <a href="#">
-            <img class="img-fluid rounded mb-3 mb-md-0" src="http://placehold.it/500x300" alt=""></img>
-          </a>
-        </Col>
-        <Col md="4">
-          <h3>Quiz Zero</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium veniam exercitationem expedita laborum at voluptate. Labore, voluptates totam at aut nemo deserunt rem magni pariatur quos perspiciatis atque eveniet unde.</p>
-          <p class="byline text-right">by Jamez Kurosami</p>
-          </Col>
-          <hr/>
-      </Row> )
-
-}
-
-export default QuisListItem;
+export default QuizListItem;
