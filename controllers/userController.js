4fa2f37718210e5890f03a1371d08a79d08da804
Jenny Valle <jennyvalle@Jennys-MacBook-Air.local>
2018-05-07 09:28:58 -0400

    Trying to add user

--- controllers/userController.js
+++ controllers/userController.js
@@ -35,10 +35,12 @@ module.exports = {
 
                    if(result){
 
+                       console.log("user found");
                        res.send(result);
 
                    } else {
 
+                        console.log("creating new user");
                         db.User.create(req.body)
                                .then(success => res.send(req.body))
                                .catch(err => res.send(err));
